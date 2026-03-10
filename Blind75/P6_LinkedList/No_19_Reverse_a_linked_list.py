34f61e40b9106e15cc3d7045b2057197c2ae0aea
Namit S Gouranna <career.namitgouranna@gmail.com>
2026-03-10 22:31:26 +0530

    Added linked list folder and problem 19 reversing a linked list

---
+++ Blind75/P6_LinkedList/No_19_Reverse_a_linked_list.py
@@ -0,0 +1,30 @@
+# Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+# Example 1:
+
+# Input: head = [0,1,2,3]
+
+# Output: [3,2,1,0]
+# Example 2:
+
+# Input: head = []
+
+# Output: []
+# Constraints:
+
+# 0 <= The length of the list <= 1000.
+# -1000 <= Node.val <= 1000
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    cur, prev = head, None
+    while cur:
+        next_node = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next_node
+    return prev
